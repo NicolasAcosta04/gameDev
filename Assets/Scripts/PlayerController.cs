d7ed48ff6cf31e966e11274447b4533280ae0d23
Nicolas Acosta <n.acosta@se22.qmul.ac.uk>
2024-09-30 14:37:34 +0100

    completed all environment and script changes up to 4.2.3 in lab2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
     public Vector2 moveValue;
     public float speed;
+    private Vector3 lastPos;
+    private int count;
+    private int numPickUps = 6;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+    public TextMeshProUGUI playerPosText;
+    public TextMeshProUGUI playerVelocityText;
+
+
+    void Start() { 
+        count = 0;
+        winText.text = "";
+        lastPos = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
+        SetCountText ();
+    }
 
     void OnMove(InputValue value)
     {
@@ -16,8 +32,31 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+
+        var currentPos = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
+        playerPosText.text = currentPos.ToString("0.00");
+        var velocity = (currentPos - lastPos) / Time.fixedDeltaTime;
+        playerVelocityText.text = velocity.magnitude.ToString("0.00") + "m/s";
+
+        lastPos = currentPos;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText() {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickUps) {
+            winText.text = "You win!";
+        }
     }
 }
